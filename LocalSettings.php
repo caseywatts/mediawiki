19ea656955d7b34c25d1ade30d9d236de32205ff
Casey Watts <casey.s.watts@gmail.com>
2017-06-12 23:01:55 -0400

    even more debugging enabled

--- LocalSettings.php
+++ LocalSettings.php
@@ -138,3 +138,9 @@ $CASAuth = array_merge($CASAuth, array(
 error_reporting( -1 );
 ini_set( 'display_errors', 1 );
 $wgShowExceptionDetails = true;
+$wgDebugToolbar = true;
+$wgShowDebug = true;
+$wgDevelopmentWarnings = true;
+$wgShowSQLErrors = true;
+$wgDebugDumpSql  = true;
+$wgShowDBErrorBacktrace = true;
